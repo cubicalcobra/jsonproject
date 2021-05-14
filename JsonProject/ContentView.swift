14c9e00feb866db978adaa624b87bf08603dc926
Stian Jacobsen <sj@spear.no>
2021-05-14 19:47:13 +0200

    Initial Commit

---
+++ JsonProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JsonProject
+//
+//  Created by Stian Jacobsen on 14/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JsonProject/JsonProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonProjectApp.swift
+//  JsonProject
+//
+//  Created by Stian Jacobsen on 14/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct JsonProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
