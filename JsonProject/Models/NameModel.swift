ba3e00b5cab2bce19fc72b3b4f296032d209878a
Stian Jacobsen <sj@spear.no>
2021-05-14 20:29:16 +0200

    - create json datafile - create NameModel

---
+++ JsonProject/Models/NameModel.swift
@@ -0,0 +1,17 @@
+//
+//  NameModel.swift
+//  JsonProject
+//
+//  Created by Stian Jacobsen on 14/05/2021.
+//
+
+import Foundation
+
+class Name: Identifiable, Decodable {
+    
+    var id:UUID?
+    var Name:String
+    var Contact:Int?
+    
+    
+}
